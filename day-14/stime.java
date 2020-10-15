2a0a5819f4b02d0fe77708de2ed1bc7f61a7a8c8
Naresh <ff.jonnala@gmail.com>
2020-10-15 10:49:57 +0530

    1. java test program.

---
+++ day-14/stime.java
@@ -0,0 +1,12 @@
+//second time java
+
+// syntax: declare a variable in java
+//char name = "Naresh"
+
+public class stime{
+    public static void main(String[] args){
+//        double pi=3.14;
+        double pi = 3.141592653;
+        System.out.println("The value of pi is:" + pi);
+    }
+}
