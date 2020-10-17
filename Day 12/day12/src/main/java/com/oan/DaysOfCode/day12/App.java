cc277b25f8fd2980a4cc8c7bbbbc0e16087d1296
iHarishKumar <gharishkumar.2023@gmail.com>
2020-10-17 12:04:41 +0530

    [OAN][100DaysOfCode][Day12] Creating a first Noob Blockchain

---
+++ Day 12/day12/src/main/java/com/oan/DaysOfCode/day12/App.java
@@ -0,0 +1,13 @@
+package com.oan.DaysOfCode.day12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
