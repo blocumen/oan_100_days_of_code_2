7bebdfc37bf087209acb14800e3d5730e373cc2e
jrp2025 <braveflowerj@gmail.com>
2020-10-19 15:04:31 +0530

    file added

---
+++ src/BlockchainDay17/A.java
@@ -0,0 +1,38 @@
+package BlockchainDay17;
+
+
+
+    public class A
+    {
+        protected void msg(){System.out.println("Hello");}
+    }
+/*
+    //public access modifier :-
+
+    public class A
+    {
+        public void msg(){System.out.println("Hello");}
+    }
+
+ */
+
+/*
+        //private access modifier
+
+        private A()
+        {}//private constructor
+        void msg()
+        {
+            System.out.println("Hello java");
+        }
+        public class Simple
+        {
+            public static void main(String args[])
+            {
+                A obj=new A();//Compile Time Error
+            }
+        }
+
+
+ */
+
---
+++ src/BlockchainDay17/B.java
@@ -0,0 +1,27 @@
+package BlockchainDay17;
+
+import BlockchainDay17.*;
+
+class B extends A
+{
+    public static void main(String args[])
+    {
+        B obj = new B();
+        obj.msg();
+    }
+}
+
+/*
+    //public access modifier:-
+
+    class B
+    {
+        public static void main(String args[])
+        {
+            A obj = new A();
+            obj.msg();
+        }
+    }
+
+ */
+
