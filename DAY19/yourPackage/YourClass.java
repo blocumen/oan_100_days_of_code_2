352e51e95353bac06b5e9c603221553007ee88f5
Aditya Kakde <aditya.kakde@ad.infosys.com>
2020-10-19 09:06:57 +0530

    DAY19_Assignment

---
+++ DAY19/yourPackage/YourClass.java
@@ -0,0 +1,12 @@
+
+package yourPackage;
+import myPackage.MyClass;
+
+public class YourClass {
+
+	public static void main(String[] args) {
+		MyClass myClass = new MyClass();
+		myClass.getNames();
+	}
+
+}
