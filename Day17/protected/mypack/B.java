de4f53f6d486ea3311f94168324002c577234d8a
Praveen Vidyavastu <PRAVEENGANDHIVIDYAVASTU@eurofins.com>
2020-10-19 19:08:31 +0530

    protected access specifier commit 2

--- Day17/protected/mypack/B.java
+++ Day17/protected/mypack/B.java
@@ -3,7 +3,7 @@ import pack.*;
 
 public class B extends A{
     public static void main(String[] args) {
-        B obj = new B(); // Compile time error
-        obj.msg(); // Compile time error
+        B obj = new B();
+        obj.msg();
     }
 }
