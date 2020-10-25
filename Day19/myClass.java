5a22abf1df8a8812987ad443ef833459eca8a5b0
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-10-25 16:48:48 +0530

    Create myClass.java

---
+++ Day19/myClass.java
@@ -0,0 +1,11 @@
+package yourPackage;
+import myPackage.MyClass;
+
+public class YourClass {
+
+	public static void main(String[] args) {
+		MyClass myClass = new MyClass();
+		myClass.getNames();
+	}
+
+}
