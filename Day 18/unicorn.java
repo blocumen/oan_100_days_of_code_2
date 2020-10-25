098316230ac0adefb9bffdb338217a17a24e0182
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-10-25 16:44:40 +0530

    Create unicorn.java

---
+++ Day 18/unicorn.java
@@ -0,0 +1,20 @@
+class Unicorn {
+// properties
+private int height = 170;
+public String power = "Double.infinity";
+// methods
+private static void sleep() {
+int minutesToSleep = 120;
+system.out.println(minutesToSleep);
+// system.out.println(minutesToRun);  error
+system.out.println(height);
+system.out.println(power);
+}
+public static void run() {
+int minutesToSleep = 60;
+// system.out.println(minutesToSleep); error
+system.out.println(minutesToRun);
+system.out.println(height);
+system.out.println(power);
+}
+}
