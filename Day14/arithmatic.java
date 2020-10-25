ff841b0a8216a81e65d9b4e94cad9a840ea401dc
Anshul Rai <41705919+auralshin@users.noreply.github.com>
2020-10-25 12:01:17 +0530

    day 14 assignment

---
+++ Day14/arithmatic.java
@@ -0,0 +1,22 @@
+class Arithmatic {
+        public static int num1 = 20, num2 = 10, mult = 0, add = 0, sub = 0;
+        public static double div = 0;
+
+        public static void main(String[] args) {
+
+                mult = num1 * num2;
+
+                div = num1 / num2;
+
+                add = num1 + num2;
+
+                sub = num1 - num2;
+                System.out.println("num1 = " + num1);
+                System.out.println("num2 = " + num2);
+                System.out.println("Multiplication = " + mult);
+                System.out.println("Division = " + div);
+                System.out.println("Addition = " + add);
+                System.out.println("Subtraction = " + sub);
+
+        }
+}
