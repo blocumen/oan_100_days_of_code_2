99d5539bc1b908b41b5e7a837a0299ea7f752cd5
santhosh <santhoshkm@gmail.com>
2020-10-28 07:10:54 +0530

    day18

---
+++ day18/unicorn.java
@@ -0,0 +1,24 @@
+class Unicorn
+{
+	int height = 170;
+	String power = "Double.infinity";
+
+	//methods
+	public void sleep() {
+		int minutesToSleep = 120;
+
+		System.out.println(minutesToSleep);
+		System.out.println(minutesToRun);
+		System.out.println(height);
+		System.out.println(power);
+	}
+
+	public void run() {
+		int minutesToRun = 60;
+
+		System.out.println(minutesToSleep);
+		System.out.println(minutesToRun);
+		System.out.println(height);
+		System.out.println(power);
+	}
+}
