cc0b5f6d13324f3062fb28866b9802f862cb7614
Ahmed Farook <ahmedgaziyani@gmail.com>
2020-10-28 13:13:19 +0530

    Importing Package from another package

---
+++ Day19/CallingClassOutsidePackage.java
@@ -0,0 +1,12 @@
+package calc;
+import myPackage.MyClass;
+
+public class Classes {
+
+    public static void main(String[] args)
+    {
+        MyClass ob = new MyClass();
+        String names = ob.getNames("Ahmed");
+    }
+
+}
---
+++ Day19/MyClass.java
@@ -0,0 +1,21 @@
+package myPackage;
+
+public class MyClass {
+
+    public String getNames(String name) {
+        //String name = "HI";
+        System.out.println("My Name is " + name);
+
+        return name;
+    }
+}
+//    public static void  main(String[] args) {
+//
+//        MyClass ob = new MyClass();
+//        ob.getNames("Ahmed");
+//
+//    }
+//
+//
+//}
+
