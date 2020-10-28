6baf69b58643e836fc3f0239b377d79a5c43ef24
santhosh <santhoshkm@gmail.com>
2020-10-28 08:18:00 +0530

    day21

---
+++ day21/myclass.java
@@ -0,0 +1,11 @@
+class myClass {
+	public static void main(String[] args) {
+		byte test = 10;
+
+		System.out.println("Byte" + test);
+		System.out.println("Byte" + (short)test);
+		System.out.println("Byte" + (int)test);
+		System.out.println("Byte" + (float)test);
+		System.out.println("Byte" + (double)test);
+	}
+}
