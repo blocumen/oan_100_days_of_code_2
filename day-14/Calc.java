9545e2f7c1365515c4f964f30b0e318509531950
Naresh <ff.jonnala@gmail.com>
2020-11-01 15:53:43 +0530

    1. day-14 update.

--- day-14/Calc.java
+++ day-14/Calc.java
@@ -1,6 +1,26 @@
 import java.util.Scanner;
 
 public class Calc{
+
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+    public static double sub(double a, double b){
+        return a-b;
+    }
+
+    public static double multi(double a, double b){
+        return a*b;
+    }
+
+    public static double division(double a, double b){
+        if (b == 0){
+            return 0.0;
+        }
+        return a/b;
+    }
+
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter 2 numbers: ");
@@ -8,30 +28,19 @@ public class Calc{
         double first = reader.nextDouble();
         double second = reader.nextDouble();
 
-//        public static int add(int a, int b){
-//            return a+b;
-//        }
-//
-//        public static int sub(int a, int b){
-//            return a-b;
-//        }
-//
-//        public static int multi(int a, int b){
-//            return a*b;
-//        }
-//
-//        public static int division(int a, int b){
-//            if (b == 0){
-//                return null;
-//            }
-//            return a/b;
-//        }
+//        System.out.println("Sum of 2 numbers is: "+ (first + second));
+        System.out.println("Sum of 2 numbers is: "+ add(
+                first, second));
+//        System.out.println("Sub of 2 numbers is: "+ (first - second));
+        System.out.println("Sub of 2 numbers is: "+ sub(
+                first, second));
 
-        System.out.println("Sum of 2 numbers is: "+ (first + second));
-        System.out.println("Sub of 2 numbers is: "+ (first - second));
-        System.out.println("Mul of 2 numbers is: "+ (first * second));
-        System.out.println("Div of 2 numbers is: "+ (first / second));
+//        System.out.println("Mul of 2 numbers is: "+ (first * second));
+        System.out.println("Mul of 2 numbers is: "+ multi(
+                first, second));
 
-
+//        System.out.println("Div of 2 numbers is: "+ (first / second));
+        System.out.println("Div of 2 numbers is: "+ division(
+                first, second));
     }
 }
