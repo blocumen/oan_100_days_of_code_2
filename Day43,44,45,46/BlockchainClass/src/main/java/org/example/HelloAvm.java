049d58d1f9efd97d49fbf87247bd11fb7440f9f3
Tyler <mailtosanjay3103@gmail.com>
2020-11-16 16:17:34 +0530

    tested blockchain class

---
+++ Day43,44,45,46/BlockchainClass/src/main/java/org/example/HelloAvm.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import avm.Address;
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+
+public class HelloAvm
+{
+    private static Address owner ;
+   static {
+        owner = Blockchain.getCaller();
+   }
+   private  static void onlyOwner(){
+       Blockchain.require(Blockchain.getCaller().equals(owner));
+   }
+
+   @Callable
+   public static void checkOwnercalled(){
+       onlyOwner();
+       Blockchain.println("func called ");
+   }
+
+    @Callable
+    public  static void addressOffuncCaller(){
+
+        Address functioncaller = Blockchain.getCaller();
+        Blockchain.println("function caller is "+functioncaller.toString());
+    }
+
+    @Callable
+    public static void calculateGasofFunc(){
+       long initialGas = Blockchain.getRemainingEnergy();
+       Blockchain.println("intial energy "+initialGas);
+       int res = 5+23;
+       Blockchain.println("Remaining energy is :"+Blockchain.getRemainingEnergy());
+
+    }
+    @Callable
+    public static void getCode(Address address){
+       Blockchain.println("The code size is "+ String.valueOf(Blockchain.getCodeSize(address)));
+    }
+
+    @Callable
+    public static void emitEvent() {
+        String eventTopic = "Emit Event"; // topic
+        Address eventCaller = Blockchain.getCaller(); // data
+        Blockchain.log(eventTopic.getBytes(), eventCaller.toByteArray());
+    }
+    @Callable
+    public static long getBlockNumber(String newStr) {
+        Blockchain.println("Block number to be mined: "+Blockchain.getBlockNumber());
+        return Blockchain.getBlockNumber();
+    }
+
+    @Callable
+    public static void getCallerAddress() {
+        Blockchain.println("func caller is "+Blockchain.getCaller());
+    }
+
+}
