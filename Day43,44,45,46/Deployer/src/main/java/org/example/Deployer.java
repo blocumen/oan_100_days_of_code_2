7256326376829839135e266ffcc544451a15fcec
Tyler <mailtosanjay3103@gmail.com>
2020-11-16 16:37:54 +0530

    deployer checking contract pushed

---
+++ Day43,44,45,46/Deployer/src/main/java/org/example/Deployer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import avm.Address;
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+
+public class Deployer
+{
+    private static Address owner;
+   static {
+       owner = Blockchain.getCaller();
+   }
+   private static void onlyOwner(){
+       Blockchain.require(owner.equals(Blockchain.getCaller()));
+   }
+   @Callable
+   public static void func1(){
+       onlyOwner();
+       Blockchain.println("Deployer of this smart contract has called the function");
+   }
+}
