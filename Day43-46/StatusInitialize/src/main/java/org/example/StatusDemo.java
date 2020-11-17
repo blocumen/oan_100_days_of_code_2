51bed26df45153f690c8515925d57c624a4d5e0f
Praveen Vidyavastu <PRAVEENGANDHIVIDYAVASTU@eurofins.com>
2020-11-17 13:53:22 +0530

    Day 45 Status Commit3

--- Day43-46/StatusInitialize/src/main/java/org/example/StatusDemo.java
+++ Day43-46/StatusInitialize/src/main/java/org/example/StatusDemo.java
@@ -4,6 +4,20 @@
 
     Create a smart contract class. Add an initializable variable called status and provide a getter method
     that returns the value set during initialization.
+
+    Pass -T <some_value> in deployment arguments
+      -I int
+      -J long
+      -S short
+      -C char
+      -F float
+      -D double
+      -B byte
+      -Z boolean
+      -A Address
+      -T String
+
+    To deploy using terminal  mvn aion4j:deploy -Dargs="-Z true"
  */
 package org.example;
 
