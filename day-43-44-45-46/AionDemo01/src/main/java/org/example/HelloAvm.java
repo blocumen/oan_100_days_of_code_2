fff7db143313ab8a871faee9397f7252bbecac8e
Naresh <ff.jonnala@gmail.com>
2020-11-26 21:05:31 +0530

    day41- assignment 1.

---
+++ day-43-44-45-46/AionDemo01/src/main/java/org/example/HelloAvm.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import avm.Blockchain;
+import org.aion.avm.tooling.abi.Callable;
+
+public class HelloAvm {
+    private static String myStr = "Hello AVM";
+
+    @Callable
+    public static void sayHello() {
+        Blockchain.println("Hello Avm");
+    }
+
+    @Callable
+    public static String greet(String name) {
+        return "Hello " + name;
+    }
+
+    @Callable
+    public static String getString() {
+        Blockchain.println("Current string is " + myStr);
+        return myStr;
+    }
+
+    @Callable
+    public static void setString(String newStr) {
+        myStr = newStr;
+        Blockchain.println("New string is " + myStr);
+    }
+
+    @Callable
+    public static int Mul(int num1) {
+        return (num1 * 10);
+    }
+
+}
