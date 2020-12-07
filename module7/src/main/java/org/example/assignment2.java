d3d381ae82cb32ea184eea5d5f38d4a00b921e78
Naresh <ff.jonnala@gmail.com>
2020-12-07 21:30:41 +0530

    1. module 7 assignment2 @initializable variable getter function and return initializable value.

--- module7/src/main/java/org/example/assignment2.java
+++ module7/src/main/java/org/example/assignment2.java
@@ -5,10 +5,18 @@ import org.aion.avm.tooling.abi.Initializable;
 
 public class assignment2 {
     @Initializable
-    private static String myString = "Test message";
+    private static String myString;
 
     @Callable
     public static String getter(){
         return myString;
     }
 }
+
+/*
+* 1. Create a smart contract class. Add an initializable variable
+* called status and provide a getter method that returns the value
+* set during initialization.
+*
+* While deploy Aion VM >> should pass argument {-T "<value>"}
+* */
