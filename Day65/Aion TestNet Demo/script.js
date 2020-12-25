8ff27d6017c4956ec221941d851bfd692f8a9a4d
Praveen Vidyavastu <praveentyk@gmail.com>
2020-12-25 12:46:26 +0530

    Update script.js

--- Day65/Aion TestNet Demo/script.js
+++ Day65/Aion TestNet Demo/script.js
@@ -1,5 +1,5 @@
 const web3 = new Web3(new Web3.providers.HttpProvider("https://aion.api.nodesmith.io/v1/mastery/jsonrpc?apiKey=f0345c7bd03f49938d07cabeaf6986f5"));
-const account = web3.eth.accounts.privateKeyToAccount("bb7032d0b3ae5cce16f7f8fbd6c16bb43fffc2a30805a6c683bacace96963f0b424a97b6e17dc2acd604bb503847c99209dc83333ebc8b8daea9ddebaa047f10");
+const account = web3.eth.accounts.privateKeyToAccount("bb7032d0b3ae5cce16f7f8fbd6c16bb43fffc2a30805a6c683bacace96963f0b424a97b6e17dc2acd604bb503847c99209dc8333ebc8b8daea9ddebaa047f10");
 const contractAddress = "0xa0ce15297c16356fd4fcddc0cb38c8a0bd4e161759275a9fa9c43221dd3c8c97";
 const methodName = "greet";
 
